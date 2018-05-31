bdbe862f37270db985f76f82dc28c777a389f10e
Satoshi Miyazaki <>
2018-06-01 00:12:50 +0900

    レクチャー3：gulpとローカル開発環境の構築

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp        = require('gulp');
+var watch       = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', function () {
+    watch('./**/*.html', function () {
+        browserSync.reload();
+    });
+});
+
+gulp.task('default',['browser-sync', 'watch']);
