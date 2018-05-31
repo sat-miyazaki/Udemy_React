8d9167f3f1bccf706c0b67b9926c4d6400c7d25b
Satoshi Miyazaki <>
2018-06-01 01:58:22 +0900

    レクチャー4：はじめてのReact

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function () {
-    watch('./**/*.html', function () {
+    watch(['./**/*.html', './js/**/*.js'], function () {
         browserSync.reload();
     });
 });
