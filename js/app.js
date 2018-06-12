a13635acd479ad6b68b0ee91565136b682aa956b
Satoshi Miyazaki <>
2018-06-12 22:55:06 +0900

    レクチャー6：JSXの基本

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
+
+const message1 = <i>Hello React!!</i>;
+const message2 = <strong>Hello Redux!!</strong>;
+
+const flag = true;
+
+function getMessage() {
+  if (flag) {
+    return message1;
+  } else {
+    return message2;
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div>
+    <h1>{getMessage()}</h1>
+    <p>React + Redux</p>
+  </div>,
   document.getElementById('root')
 );
