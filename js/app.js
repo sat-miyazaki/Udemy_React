ff310c6c2095ecdd2d69201d97db230bce40cc0e
Satoshi Miyazaki <>
2018-06-12 23:19:50 +0900

    レクチャー7：画面レイアウトの作成

--- js/app.js
+++ js/app.js
@@ -1,21 +1,10 @@
-
-const message1 = <i>Hello React!!</i>;
-const message2 = <strong>Hello Redux!!</strong>;
-
-const flag = true;
-
-function getMessage() {
-  if (flag) {
-    return message1;
-  } else {
-    return message2;
-  }
-}
-
 ReactDOM.render(
-  <div>
-    <h1>{getMessage()}</h1>
-    <p>React + Redux</p>
+  <div className="wrap">
+    <div className="side-area"></div>
+    <div className="main-area">
+      <header className="header"></header>
+      <footer className="footer"></footer>
+    </div>
   </div>,
   document.getElementById('root')
 );
